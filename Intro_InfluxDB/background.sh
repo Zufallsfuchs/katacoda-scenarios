cf7f19f962df6af6b5c74fcfb1fb61154926ea4f
Robin Köhler <Robin_Spiele1990@web.de>
2021-03-23 23:16:50 +0100

    Scripts

--- Intro_InfluxDB/background.sh
+++ Intro_InfluxDB/background.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-
 docker pull influxdb:2.0.4
-
 sleep 5
-
 echo "done" >> /opt/.backgroundfinished
--- Intro_InfluxDB/step2_back.sh
+++ Intro_InfluxDB/step2_back.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-
 echo "Waiting for docker to pull InfluxDB"; while [ ! -f /opt/.backgroundfinished ] ; do sleep 2; done; echo "Ready to go!"
