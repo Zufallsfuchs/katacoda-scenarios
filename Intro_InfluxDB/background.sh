0ea1808a29928ba756fabf84367027c987cd8dc7
Robin Köhler <Robin_Spiele1990@web.de>
2021-03-23 23:14:11 +0100

    Docker pull
    Get docker pull to work

---
+++ Intro_InfluxDB/background.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+docker pull influxdb:2.0.4
+
+sleep 5
+
+echo "done" >> /opt/.backgroundfinished
---
+++ Intro_InfluxDB/step2_fore.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo "Waiting for docker to pull InfluxDB"; while [ ! -f /opt/.backgroundfinished ] ; do sleep 2; done; echo "Ready to go!"
